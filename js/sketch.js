c70afa2a9cca51ddd3afc6695b1c653d2ec83812
yasai wang <yasaiwang@yasaideMacBook-Pro.local>
2018-09-13 08:46:20 +0800

    update

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,9 @@
 var myCanvas;
 var r;
 var x1,y1,x2,y2,x3,y3,x4,y4,sx,sy;
-
-
+var radius;
+var mode;
+// 
 function setup() {
   myCanvas = createCanvas(windowWidth, windowHeight);
   if(width > height){
@@ -13,8 +14,9 @@ function setup() {
 }
 
 
+
 function draw() { 
-  frameRate(0.00001);
+  frameRate(0.001);
   if(options.SavePNG == true){
     background(0,0,0,0);
   }else{
@@ -47,27 +49,8 @@ function speedline(r){
     translate(width/2,height/2);
     noFill();
 
-    if(options.Style == 'style7'){    
-      beginShape();
-      vertex(options.CenterX,options.CenterY+height/1.6);
-      x3 = sin(radians(i))*r+random(options.Offset);
-      y3 = cos(radians(i))*r+random(options.Offset);
-      x1 = x3/1.5;
-      x2 = x1;
-      y1 = (-r/7+y3)/2;
-      y2 = y1;
-      stroke(options.Color[0],options.Color[1],options.Color[2],100);
-      bezierVertex(x1,y1,x2,y2,x3,y3);
-      endShape();
-
-      fill(options.Color);
-      noStroke();
-      ellipse(x3,y3,5,5);
-    }
-
 
     if(options.Style == 'style1'){   
-
       x3 = sin(radians(i))*r+random(options.Offset);
       y3 = cos(radians(i))*r+random(options.Offset);
 
@@ -87,7 +70,6 @@ function speedline(r){
       x2 = x3;
       y2 = y3-sy;
     }
-
     else if( i >270 && i <360){
       sx = map(i, 270, 360, -r/1.5, 0);
       sy = map(i, 270, 360, r/20, r);
@@ -97,7 +79,6 @@ function speedline(r){
       y2 = y3;
     }
 
-
     else if(i>360 && i<450){
      sx = map(i, 360, 450, 0, r/1.5);
      sy = map(i, 360, 450, r/4,r/20);
@@ -106,7 +87,6 @@ function speedline(r){
      x2 = x3+sx/10;
      y2 = y3;
    }
-
 
    else if(i >450 && i <500 ){
     sx = map(i, 450, 500, r/1.5, r/5);
@@ -129,9 +109,8 @@ function speedline(r){
   noFill();
   push();
   beginShape();
-  // rotate(-PI/6);
   vertex(options.CenterX,options.CenterY);
-  stroke(options.Color[0],options.Color[1],options.Color[2],100);
+  stroke(options.Color[0],options.Color[1],options.Color[2],120);
   bezierVertex(x1,y1,x2,y2,x3,y3);
   endShape();
 
@@ -140,8 +119,116 @@ function speedline(r){
   ellipse(x3,y3,5,5);
   pop();
 } 
+if(options.Style == 'style1'){  
+
+  if( i < 220){
+    var r1 = map(i,180,260,r*0.88,r*0.72);
+    x3 = sin(radians(i))*r1+random(options.Offset);
+    y3 = cos(radians(i))*r1+random(options.Offset);
+    sx = map(i, 180, 220, -r1/5, -r1/30);
+    sy = map(i, 180, 220, 0, 0);
+    x1 = x3*0.5;
+    y1 = options.CenterY+sy;
+    x2 = x3*0.8;
+    y2 = y3;
+    noFill();
+    push();
+    beginShape();
+    vertex(options.CenterX,options.CenterY);
+    stroke(options.Color[0],options.Color[1],options.Color[2],120);
+    bezierVertex(x1,y1,x2,y2,x3,y3);
+    endShape();
+
+    fill(options.Color);
+    noStroke();
+    ellipse(x3,y3,5,5);
+    pop();
+  }
+  else if( i >220 && i <360){
+    if(i%5<2){
+     x3 = sin(radians(i))*r*0.45+random(options.Offset)-random(options.Radius/5);
+    y3 = cos(radians(i))*r*0.15+random(options.Offset)-random(options.Radius/2);
+    sx = map(i, 220, 360, -r1/3.3,0);
+    sy = map(i, 220, 360, -10, 10);
+    x1 = sx;
+    y1 = options.CenterY;
+    x2 = x3+sx/10;
+    y2 = y3;
+    noFill();
+    push();
+    beginShape();  
+    push();
+    vertex(options.CenterX,options.CenterY);
+    stroke(options.Color[0],options.Color[1],options.Color[2],120);
+    bezierVertex(x1,y1,x2,y2,x3,y3);
+    endShape();
+
+    fill(options.Color);
+    noStroke();
+    ellipse(x3,y3,5,5);
+    pop();
+
+    pop();
+    }
+    
+  }
+
+  else if(i>360 && i<500){
+   if(i%5<2){    
+    x3 = sin(radians(i))*r*0.45+random(options.Offset)+random(options.Radius/5);
+    y3 = cos(radians(i))*r*0.15+random(options.Offset)-random(options.Radius/2);
+    sx = map(i, 220, 500, 0,r1/3.3);
+    sy = map(i, 220, 500, -10, 10);
+    x1 = sx;
+    y1 = options.CenterY;
+    x2 = x3+sx/10;
+    y2 = y3;
+    noFill();
+    push();
+    beginShape();  
+    push();
+    vertex(options.CenterX,options.CenterY);
+    stroke(options.Color[0],options.Color[1],options.Color[2],120);
+    bezierVertex(x1,y1,x2,y2,x3,y3);
+    endShape();
+
+    fill(options.Color);
+    noStroke();
+    ellipse(x3,y3,5,5);
+    pop();
+
+    pop();
+  }
+
+}
+
+else if(i >500  ){
+    var r1 = map(i,500,600,r*0.88,r*0.72);
+    x3 = sin(radians(i))*r*0.95+random(options.Offset);
+    y3 = cos(radians(i))*r*0.87+random(options.Offset);
+    sx = map(i, 500, 600,r1/10, r1/5);
+    sy = map(i, 500, 600, 0, -r1/10);
+    x1 = x3*0.5;
+    y1 = options.CenterY+sy;
+    x2 = x3*0.8;
+    y2 = y3;
+    noFill();
+    push();
+    beginShape();
+    vertex(options.CenterX,options.CenterY);
+    stroke(options.Color[0],options.Color[1],options.Color[2],120);
+    bezierVertex(x1,y1,x2,y2,x3,y3);
+    endShape();
+
+    fill(options.Color);
+    noStroke();
+    ellipse(x3,y3,5,5);
+    pop();
+  
+}
 
 
+}
 
 else if(options.Style == 'style2'){
   x1 = sin(radians(0))*r+random(options.Offset);
@@ -207,6 +294,7 @@ else if(options.Style== 'style4'){
   noStroke();
   ellipse(x1,y1,5,5);
 } 
+
 else if(options.Style== 'style5'){
   x1 = sin(radians(0))*r+random(options.Offset);
   y1 = cos(radians(0))*r+random(options.Offset);
@@ -233,7 +321,7 @@ else if(options.Style== 'style6'){
 
   rotate(i);
   noFill();
-  stroke(options.Color[0],options.Color[1],options.Color[2],100);
+  stroke(options.Color[0],options.Color[1],options.Color[2],150);
 
   line(x1,y1,x2,y2);
   fill(options.Color);
@@ -243,9 +331,29 @@ else if(options.Style== 'style6'){
   ellipse(x1,y1,radius,radius);
   ellipse(x2,y2,radius,radius);
 } 
+else if(options.Style == 'style7'){    
+  beginShape();
+  vertex(options.CenterX,options.CenterY+height/1.6);
+  x3 = sin(radians(i))*r+random(options.Offset);
+  y3 = cos(radians(i))*r+random(options.Offset);
+  x1 = x3/1.5;
+  x2 = x1;
+  y1 = (-r/7+y3)/2;
+  y2 = y1;
+  stroke(options.Color[0],options.Color[1],options.Color[2],100);
+  bezierVertex(x1,y1,x2,y2,x3,y3);
+  endShape();
+
+  fill(options.Color);
+  noStroke();
+  ellipse(x3,y3,5,5);
+}
 pop();
 }
 }
+
+
+
 
 
 
